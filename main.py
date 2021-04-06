36a6e0a515254b43ae68e0d79b3ea8e7ef7ff0c3
lastchancexi <james.jy.you@gmail.com>
2021-04-05 22:53:13 -0700

    Finish new rankings script

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from roster import updateroster #gets roster from yahoo api
 from fangraphsdownload import updatefangraphs #runs selenium on fangraphs in firefox to update roster
 #from yahooprojectionsmerge import merge as yahooprojectionsmerge
 
-updatefangraphs(link='https://www.fangraphs.com/projections.aspx?pos=all&stats=pit&type=rfangraphsdc&team=0&lg=all&players=0',path='hitters')
+updatefangraphs(link='https://www.fangraphs.com/projections.aspx?pos=all&stats=bat&type=rfangraphsdc&team=0&lg=all&players=0',path='hitters')
 updatefangraphs(link='https://www.fangraphs.com/projections.aspx?pos=all&stats=pit&type=rfangraphsdc&team=0&lg=all&players=0',path='pitchers')
 updatefangraphs(link='https://www.fangraphs.com/leaders.aspx?pos=all&stats=fld&lg=all&qual=0&type=0&season=2021&month=0&season1=2021&ind=0&team=0&rost=0&age=0&filter=&players=0',path='positions')
 updatefangraphs(link='https://www.fangraphs.com/leaders.aspx?pos=all&stats=fld&lg=all&qual=0&type=0&season=2020&month=0&season1=2020&ind=0&team=0&rost=0&age=0&filter=&players=0&startdate=&enddate=',path='positionsold')
@@ -20,4 +20,4 @@ with open('./auth/example.json') as json_yahoo_file:
     if 'access_token' not in (list(auths)):
             leagueauth()
 
-#updateroster(leagueid=,numteams=)
+updateroster(leagueid=,numteams=)
