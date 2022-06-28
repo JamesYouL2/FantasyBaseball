39f2d6b5dadbff71ba4be2c8e5063b6b39a45997
jyou <jyou@gsngames.com>
2022-06-28 11:09:32 -0700

    Fix sandboxing for fangraphs

--- fangraphsdownload.py
+++ fangraphsdownload.py
@@ -4,6 +4,8 @@ from selenium.webdriver.chrome.options import Options as ChromeOptions
 #object of ChromeOptions
 import pathlib
 import os
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
 
 def updatefangraphs(link='https://www.fangraphs.com/projections.aspx?pos=all&stats=bat&type=rfangraphsdc', path=r'hitters', debug=False):
     default_download_directory=str(pathlib.Path().absolute() / path)
@@ -11,26 +13,29 @@ def updatefangraphs(link='https://www.fangraphs.com/projections.aspx?pos=all&sta
     
     if os.path.exists(default_download_directory+"/FanGraphs Leaderboard.csv"):
         os.remove(default_download_directory+"/FanGraphs Leaderboard.csv")
+    caps = DesiredCapabilities().CHROME
+    caps["pageLoadStrategy"] = "eager"  #  interactive
 
     op = webdriver.ChromeOptions()
-    #add option
     op.add_argument('--no-sandbox')
     op.add_argument('--disable-dev-shm-usage')
+    op.add_argument('--disable-gpu')
+    #add option
     if debug == False:
-        op.add_argument('headless')
+        op.add_argument('--headless')
     prefs = {
-    "download.default_directory": default_download_directory,
-    "download.prompt_for_download": False,
-    "download.directory_upgrade": True,
-    "safebrowsing.enabled": True,
-    "profile.default_content_settings.popups": 0,
+        "download.default_directory": default_download_directory,
+        "download.prompt_for_download": False,
+        "download.directory_upgrade": True,
+        "safebrowsing.enabled": False,
+        "profile.default_content_settings.popups": 0,
     }
     op.add_experimental_option("prefs", prefs)
     #pass option to webdriver object
 
-    driver = webdriver.Chrome(options=op)
+    driver = webdriver.Chrome(options=op,desired_capabilities=caps)
     driver.get(link)
-    time.sleep(1)
+    time.sleep(2)
     #Close header
     try:
         driver.find_element_by_xpath('//*[@id="ezmobfooter"]/span').click()
@@ -40,7 +45,7 @@ def updatefangraphs(link='https://www.fangraphs.com/projections.aspx?pos=all&sta
     for attempt in range(4):
         try:
             driver.find_element_by_link_text('Export Data').click()
-            time.sleep(1)
+            time.sleep(2)
             break
         except:
             try:
@@ -48,6 +53,7 @@ def updatefangraphs(link='https://www.fangraphs.com/projections.aspx?pos=all&sta
             except:
                 try:
                     driver.find_element_by_link_text('Export Data').click()
+                    time.sleep(2)
                     break
                 except:
                     try:
