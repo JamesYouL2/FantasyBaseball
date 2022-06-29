8056965918afae5b08eaa0fa7f9295699737d20b
jyou <jyou@gsngames.com>
2022-06-29 07:55:07 -0700

    Move leagueauth

--- main.py
+++ main.py
@@ -11,11 +11,6 @@ config = configparser.ConfigParser()
 
 config.read('leagueid.ini')
 
-with open('./auth/example.json') as json_yahoo_file:
-    auths = json.load(json_yahoo_file)
-    if 'access_token' not in (list(auths)):
-        leagueauth()
-
 link_dict = {
     'hitters': 'https://www.fangraphs.com/projections.aspx?pos=all&stats=bat&type=rthebatx&team=0&lg=all&players=0',
     'pitchers': 'https://www.fangraphs.com/projections.aspx?pos=all&stats=pit&type=rthebat&team=0&lg=all&players=0',
@@ -29,6 +24,11 @@ for path, link in link_dict.items():
         print("Exception", e)
         updatefangraphs(link=link,path=path,debug=True)
 #updatefangraphs(link='https://www.fangraphs.com/leaders.aspx?pos=all&stats=fld&lg=all&qual=0&type=0&season=2021&month=0&season1=2021&ind=0&team=&rost=&age=&filter=&players=&startdate=&enddate=', path='positionsold', debug=True)
+
+with open('./auth/example.json') as json_yahoo_file:
+    auths = json.load(json_yahoo_file)
+    if 'access_token' not in (list(auths)):
+        leagueauth()
 
 updateroster(leagueid=str(config['DEFAULT']['leagueid']), numteams=int(
     config['DEFAULT']['numteams']))
