d24d3b6e2cfb0478b35cf3658e267a1cc86b21c2
James You <james.jy.you@gmail.com>
2025-03-17 17:06:11 -0700

    refactoring

--- league_authorization.py
+++ league_authorization.py
@@ -5,18 +5,16 @@ import json
 from json import dumps
 import datetime
 
-
 class Yahoo_Api():
     def __init__(self,
                  consumer_key,
-
-                 consumer_secret#,
-                #access_token
+                 consumer_secret
                 ):
         self._consumer_key = consumer_key
         self._consumer_secret = consumer_secret
         #self._access_token = access_token
         self._authorization = None
+        
     def _login(self):
         global oauth
         oauth = OAuth2(None, None, from_file='./auth/oauth2yahoo.json')
@@ -24,7 +22,6 @@ class Yahoo_Api():
             oauth.refresh_access_token()
 
 class Authorize():
-
     def AuthorizeLeague(self):
         # UPDATE LEAGUE GAME ID
         yahoo_api._login()
@@ -34,40 +31,30 @@ class Authorize():
         #with open('YahooGameInfo.json', 'w') as outfile:
             #json.dump(r, outfile)
             #return;
-def main():
-##### Get Yahoo Auth ####
-
-    # Yahoo Keys
-    with open('./auth/oauth2yahoo.json') as json_yahoo_file:
-        auths = json.load(json_yahoo_file)
-    yahoo_consumer_key = auths['consumer_key']
-    yahoo_consumer_secret = auths['consumer_secret']
-    #yahoo_access_token = auths['access_token']
-    #yahoo_access_secret = auths['access_token_secret']
-    json_yahoo_file.close()
-
-#### Declare Yahoo Variable ####
-
-    global yahoo_api
-    yahoo_api = Yahoo_Api(yahoo_consumer_key,
-                            yahoo_consumer_secret,
-                            #yahoo_access_token,
-                            #yahoo_access_secret)
-                                )
-#### Where the magic happen ####
-    bot = Bot(yahoo_api)
-    bot.run()
 
 class Bot():
     def __init__(self, yahoo_api):
-
         self._yahoo_api = yahoo_api
-
     def run(self):
         # Data Updates
         at = Authorize()
         at.AuthorizeLeague()
         print('Authorization Complete')
+
+def main():
+##### Get Yahoo Auth ####
+    # Yahoo Keys
+    with open('./auth/oauth2yahoo.json') as json_yahoo_file:
+        auths = json.load(json_yahoo_file)
+    yahoo_consumer_key = auths['consumer_key']
+    yahoo_consumer_secret = auths['consumer_secret']
+    json_yahoo_file.close()
+    #### Declare Yahoo Variable ####
+    global yahoo_api
+    yahoo_api = Yahoo_Api(yahoo_consumer_key, yahoo_consumer_secret,)
+#### Where the magic happen ####
+    bot = Bot(yahoo_api)
+    bot.run()
 
 if __name__ == "__main__":
     main()
