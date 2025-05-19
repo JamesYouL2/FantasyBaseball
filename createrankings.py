aabbd7f9043acfb654633f98e96aba163bf52e91
James You <james.jy.you@gmail.com>
2025-05-19 09:43:03 -0700

    fix hittersmean

--- createrankings.py
+++ createrankings.py
@@ -40,7 +40,7 @@ def exportrankings(ros=True):
     hittersmean['OPSValue'] = (hittersmean['OBPOnTeam']+hittersmean['SLGOnTeam']-.774)/.006
 
     hittersmean['AVGValue'] = ((hittersmean['H']+1768)/(hittersmean['AB']+6100)-.267)/.0024
-    hittersmean['value'] = hittersmean['OPSValue']+hittersmean['R']/8.92+hittersmean['HR']/8.0+hittersmean['RBI']/8.92+hittersmean['SB']/7.0+hittersmean['H']/9.0
+    hittersmean['value'] = hittersmean['OPSValue']+hittersmean['R']/8.92+hittersmean['HR']/8.0+hittersmean['RBI']/8.92+hittersmean['SB']/6.0+hittersmean['H']/9.0
 
     positions['Pos']=positions['Pos'].replace(['LF','CF','RF'],'OF')
     positionsold['Pos']=positionsold['Pos'].replace(['LF','CF','RF'],'OF')
